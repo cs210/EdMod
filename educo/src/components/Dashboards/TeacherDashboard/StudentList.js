02e51b6633b6bd889547c35f083c5aa98781e97a
Austin Chow <archow@stanford.edu>
2020-03-02 20:36:27 -0800

    worked on navbar

--- educo/src/components/Dashboards/TeacherDashboard/StudentList.js
+++ educo/src/components/Dashboards/TeacherDashboard/StudentList.js
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
-import StudentSummary from "./StudentSummary";
+// import React, { Component } from "react";
+// import StudentSummary from "./StudentSummary";
 
-const StudentList = () => {
-  return (
-    <div className="project-list section">
-      <StudentSummary />
-      <StudentSummary />
-      <StudentSummary />
-      <StudentSummary />
-    </div>
-  );
-};
+// const StudentList = () => {
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <StudentSummary />
+//         <StudentSummary />
+//       </div>
+//       <div className="row">
+//         <StudentSummary />
+//         <StudentSummary />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default StudentList;
+// export default StudentList;
