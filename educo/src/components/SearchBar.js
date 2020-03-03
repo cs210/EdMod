ec4a6276719a351be640f90f80f3af0344a93bdc
John Chuter <jchuter94@gmail.com>
2020-03-03 09:23:06 -0800

    Implement basic search form for QA thread searching; display search bar with text entry.

---
+++ educo/src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+/*
+add QA threads to filter:
+  1. dummy data
+  2. results UI
+  3. filtering (state, etc.)
+*/
+export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+  }
+
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
+      </form>
+    );
+  }
+}
