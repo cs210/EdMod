de25078b30192fc9916662e2c6c4d57d069bcead
John Chuter <jchuter94@gmail.com>
2020-03-03 09:23:06 -0800

    Implement basic search form for QA thread searching; display search bar with text entry.

--- educo/src/components/Dashboards/QuestionDashboard/QuestionDashboard.js
+++ educo/src/components/Dashboards/QuestionDashboard/QuestionDashboard.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
+import SearchBar from '../../SearchBar.js';
 
 class QuestionDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+    };
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+
   render() {
     return (
       <div className="dashboard container">
-        <div classname="row">
-          <div classname="col s12 m6">
-            <h1> Hello </h1>
-          </div>
-          <div classname="col s12 m5 offset-m1"></div>
-        </div>
+        <SearchBar
+          filterText={this.state.filterText}
+          onFilterTextChange={this.handleFilterTextChange}
+        />
       </div>
+
     );
   }
 }
 
 export default QuestionDashboard;
+
+// <div classname="row">
+// <div classname="col s12 m6">
+// </div>
+// </div>
+// <div classname="col s12 m5 offset-m1"></div>
