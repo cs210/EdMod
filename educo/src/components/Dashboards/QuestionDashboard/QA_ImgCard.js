b4a32da9a595170338c5f8334cd9bd09c2b4cc81
itbush <itbush@cs.stanford.edu>
2020-03-06 12:50:40 -0800

    added clip icon to attached files

--- educo/src/components/Dashboards/QuestionDashboard/QA_ImgCard.js
+++ educo/src/components/Dashboards/QuestionDashboard/QA_ImgCard.js
@@ -11,6 +11,7 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
 
 export default function ImgMediaCard() {
   const tileData = [
@@ -46,7 +47,8 @@ export default function ImgMediaCard() {
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
-        >
+        > 
+          <AttachFileIcon color='disabled' fontSize='small'/>
           <Typography variant="body2" color="textSecondary">Attached Files</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
