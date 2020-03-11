cea2ef0f986b0909f038c66ba0f2a2dbd8200ad1
John Chuter <jchuter94@gmail.com>
2020-03-11 16:18:17 -0700

    EngineerProfileDashboard class set up with Grid & supporting EngineerProfileCard class

--- educo/src/components/Dashboards/EngineerProfileDashboard/EngineerProfileDashboard.js
+++ educo/src/components/Dashboards/EngineerProfileDashboard/EngineerProfileDashboard.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
+import EngineerProfileCard from './EngineerProfileCard';
+
+import {
+  Container,
+  Button,
+  Grid,
+}
+from '@material-ui/core';
 
 class EngineerProfileDashboard extends Component {
   render() {
     return (
-      <div className="dashboard container">
-        <div classname="row">
-          <div classname="col s12 m6">
-            <h1> Hello </h1>
-          </div>
-          <div classname="col s12 m5 offset-m1"></div>
-        </div>
-      </div>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <EngineerProfileCard/>
+        <EngineerProfileCard/>
+      </Grid>
     );
   }
 }
 
 export default EngineerProfileDashboard;
+//
