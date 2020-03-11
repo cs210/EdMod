8c45c10acc65d5ecd48274fc7e9867597096957f
John Chuter <jchuter94@gmail.com>
2020-03-11 16:18:17 -0700

    EngineerProfileDashboard class set up with Grid & supporting EngineerProfileCard class

---
+++ educo/src/components/Dashboards/EngineerProfileDashboard/EngineerProfileCard.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Button,
+  Grid,
+  TextField,
+
+  // ListItemAvatar // TODO
+  // Avatar,
+}
+from '@material-ui/core';
+
+class EngineerProfileCard extends Component {
+
+  render() {
+    return (
+    <TextField label="hello world!" />
+    );
+  }
+}
+
+export default EngineerProfileCard;
