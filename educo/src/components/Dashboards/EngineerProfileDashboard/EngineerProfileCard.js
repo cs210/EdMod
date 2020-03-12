41776c11cd26c9107f94c33ef8610840a7c54efe
John Chuter <jchuter94@gmail.com>
2020-03-11 21:54:51 -0700

    engineer profile cards; expandable profiles with avatar, info

--- educo/src/components/Dashboards/EngineerProfileDashboard/EngineerProfileCard.js
+++ educo/src/components/Dashboards/EngineerProfileDashboard/EngineerProfileCard.js
@@ -1,20 +1,68 @@
 import React, { Component } from "react";
 import {
-  Container,
-  Button,
-  Grid,
-  TextField,
-
-  // ListItemAvatar // TODO
-  // Avatar,
+  Box,
+  Card,
+  CardContent,
+  CardActions,
+  Collapse,
+  IconButton,
+  // ExpandMoreIcon, // this fails unless imported as below
+  Avatar,
+  Typography
 }
 from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import Derpy from "../../../images/derpcat.jpg";
 
 class EngineerProfileCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+    };
+    this.handleExpandClick = this.handleExpandClick.bind(this)
+  }
 
+  handleExpandClick = () => {
+    this.setState(prevState => ({
+        isExpanded: !prevState.isExpanded
+      })
+    );
+  }
+
+  // TODO pass profile object thru props; grab avatar image, data
   render() {
     return (
-    <TextField label="hello world!" />
+      <Box width={1/4}>
+        <Card>
+          <Avatar src={Derpy} />
+
+          <Typography h1>
+            Derpy McDerpface
+          </Typography>
+
+
+          <CardActions disableSpacing>
+            <IconButton onClick={this.handleExpandClick}>
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+
+          <Collapse in={this.state.isExpanded} timeout="auto" unmountOnExit>
+            <CardContent>
+                <Typography subtitle1> Hometown: </Typography>
+                <Typography paragraph> Mexico City, MX </Typography>
+
+                <Typography subtitle1> Education: </Typography>
+                <Typography paragraph> Universidad Nacional Autónoma de Mexico </Typography>
+
+                <Typography subtitle1> About Me: </Typography>
+                <Typography paragraph> I was born and raised in el DF and I moved to the US six years ago for better oportunities and I’ve been happily living in Oakland since. I am acively involved in Techqueria, an org for Latinx in Tech where I help organize and run events. </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      </Box>
     );
   }
 }
