607de63a25e11d6dcd155d534a21113308c5fbb6
Austin Chow <archow@stanford.edu>
2020-04-28 13:25:28 -0700

    initialized firebase database + able to access database elements

--- educo/src/components/Dashboards/QuestionDashboard/QuestionDashboard.js
+++ educo/src/components/Dashboards/QuestionDashboard/QuestionDashboard.js
@@ -1,76 +1,133 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import QASidebar from './QA_Sidebar.js';
 import QADisplay from './QA_Display.js';
 import {Paper, Grid} from '@material-ui/core';
 import mockData from './mockData.js'
+import firebase from '../../../config/firebase.js'
 
-class QuestionDashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filterText: '',
-      q_id: this.props.match.params.q_id,
-    };
-    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-  }
+function GetQuestions() {
+  const [questions, setQuestions] = useState([]);
 
-  handleFilterTextChange(filterText) {
-    this.setState({
-      filterText: filterText
-    });
-  }
+  // firebase.firestore().collection('questions').add({
+  //   data: {author: "John Chuter"},
+  //   threads: {}
+  // })
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('questions')
+      .onSnapshot((snapshot) => {
+        const newQuestions = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }))
+        console.log(newQuestions)
+        setQuestions(newQuestions)
+      })
+  }, [])
 
 
-getMockList(){
-    var questions = [];
-    for(var i=0; i<3; i++){
-      var info = mockData(i);
-      var text = info.text;
-      if(text.length > 75){
-        text = text.substring(0,75) + "...";
-      }
-      questions.push([i.toString(), info.title, text])
-    }
-    return questions;
-  }
+  return questions
+}
 
-  
-  render() {
-    var list = this.getMockList();
-    console.log(this.state)
-    return (
-        <Grid
-        container
-        direction="row"
-        justify="space-between"
-        alignItems="stretch"
-        >
-        <Grid item sm={3}  alignItems="stretch"> 
-            <QASidebar
-              filterText={this.state.filterText}
-              onFilterTextChange={this.handleFilterTextChange}
-              questionList={list}
-            />
-        </Grid>
-        <Grid item sm={9} alignItems="stretch">
-          <QADisplay q_id={this.state.q_id}/>
-        </Grid>
+const QuestionDashboard = (props) => {
+  const [filterText, setFilterText] = useState('')
+  const [q_id, setq_id] = useState(props.match.params.q_id)
+
+  const list = GetQuestions();
+  console.log("list", list)
+
+  return (
+      <Grid
+      container
+      direction="row"
+      justify="space-between"
+      alignItems="stretch"
+      >
+      <Grid item sm={3}  alignItems="stretch">
+          <QASidebar
+            filterText={filterText}
+            onFilterTextChange={setFilterText}
+            questionList={list}
+          />
       </Grid>
+      <Grid item sm={9} alignItems="stretch">
+        <QADisplay q_id={q_id}/>
+      </Grid>
+    </Grid>
 
-    );
-  }
-  componentDidMount() {
-    this.setState({q_id: this.props.match.params.q_id});
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.match.params.q_id !== prevState.q_id) {
-      this.setState({q_id: this.props.match.params.q_id});
-    } 
-  }
+  );
 }
 
 export default QuestionDashboard;
+
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     filterText: '',
+  //     q_id: this.props.match.params.q_id,
+  //   };
+  //   this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+  // }
+  //
+  // handleFilterTextChange(filterText) {
+  //   this.setState({
+  //     filterText: filterText
+  //   });
+  // }
+
+
+// getMockList(){
+//     var questions = [];
+//     for(var i=0; i<3; i++){
+//       var info = mockData(i);
+//       var text = info.text;
+//       if(text.length > 75){
+//         text = text.substring(0,75) + "...";
+//       }
+//       questions.push([i.toString(), info.title, text])
+//     }
+//     return questions;
+//   }
+
+
+  // render() {
+  //   var list = this.getMockList();
+  //   console.log(this.state)
+  //   return (
+  //       <Grid
+  //       container
+  //       direction="row"
+  //       justify="space-between"
+  //       alignItems="stretch"
+  //       >
+  //       <Grid item sm={3}  alignItems="stretch">
+  //           <QASidebar
+  //             filterText={this.state.filterText}
+  //             onFilterTextChange={this.handleFilterTextChange}
+  //             questionList={list}
+  //           />
+  //       </Grid>
+  //       <Grid item sm={9} alignItems="stretch">
+  //         <QADisplay q_id={this.state.q_id}/>
+  //       </Grid>
+  //     </Grid>
+  //
+  //   );
+  // }
+
+  // componentDidMount() {
+  //   this.setState({q_id: this.props.match.params.q_id});
+  // }
+  //
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.props.match.params.q_id !== prevState.q_id) {
+  //     this.setState({q_id: this.props.match.params.q_id});
+  //   }
+  // }
+  //
+
 
 /*
 
