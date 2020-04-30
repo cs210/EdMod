e3bb203e51d91a409f772cbb490568c2b633645d
John Chuter <jchuter94@gmail.com>
2020-04-29 21:25:18 -0700

    Updates

---
+++ precache-manifest.c063f2ce74842a0d900a602e112fb705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519c0bbf971cb2c1cf6c79ac91a5c147",
+    "url": "/EdMod/index.html"
+  },
+  {
+    "revision": "93ef35264e3b1e43b29f",
+    "url": "/EdMod/static/css/main.ccc962bb.chunk.css"
+  },
+  {
+    "revision": "5ed787788a51a48b45a2",
+    "url": "/EdMod/static/js/2.f8d29250.chunk.js"
+  },
+  {
+    "revision": "f9cd4da9d44dce52ea93484a46998dba",
+    "url": "/EdMod/static/js/2.f8d29250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ef35264e3b1e43b29f",
+    "url": "/EdMod/static/js/main.bcbd23dc.chunk.js"
+  },
+  {
+    "revision": "822c3c1db172caf8de7d",
+    "url": "/EdMod/static/js/runtime-main.626b5627.js"
+  },
+  {
+    "revision": "a4100d25b0d637ffbe2995edb2dc6b8f",
+    "url": "/EdMod/static/media/derpcat.a4100d25.jpg"
+  }
+]);
