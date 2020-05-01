0b0144fe60f18fa9610d6a949f83694f207e50f2
Austin Chow <archow@stanford.edu>
2020-05-01 16:38:25 -0700

    further testing

--- educo/src/App.js
+++ educo/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Routes from "./components/Routing/Routes.js";
 import NavBar from "./components/layout/Navbar";
-import firebase from "./config/firebase.js"
+import firebase from "./config/firebase.js";
 // import TeacherDashboard from "./components/Dashboards/TeacherDashboard/TeacherDashboard.js";
 // import QuestionDashboard from "./components/Dashboards/QuestionDashboard/QuestionDashboard.js";
 // import ProgressDashboard from "./components/Dashboards/ProgressDashboard/ProgressDashboard.js";
@@ -11,15 +11,15 @@ import firebase from "./config/firebase.js"
 // import SignIn from "./components/auth/SignIn.js";
 //
 //class App extends Component {
-const App = props => {
-    return (
-      // <SignIn />
-      <BrowserRouter>
-        <div className="App">
-          <NavBar />
-          <Routes />
-        </div>
-      </BrowserRouter>
-    );
-}
+const App = (props) => {
+  return (
+    // <SignIn />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <NavBar />
+        <Routes />
+      </div>
+    </BrowserRouter>
+  );
+};
 export default App;
