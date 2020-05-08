61e66cdd775f233a965123ba4d5948d57a406e31
John Chuter <jchuter94@gmail.com>
2020-05-08 16:45:22 -0700

    add Google Analytics (init on App start); track Spotlight pageviews

--- educo/src/App.js
+++ educo/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Routes from "./components/Routing/Routes.js";
 import NavBar from "./components/layout/Navbar";
 import firebase from "./config/firebase.js";
+import ReactGA from 'react-ga';
 // import TeacherDashboard from "./components/Dashboards/TeacherDashboard/TeacherDashboard.js";
 // import QuestionDashboard from "./components/Dashboards/QuestionDashboard/QuestionDashboard.js";
 // import ProgressDashboard from "./components/Dashboards/ProgressDashboard/ProgressDashboard.js";
@@ -12,6 +13,7 @@ import firebase from "./config/firebase.js";
 //
 //class App extends Component {
 const App = (props) => {
+  ReactGA.initialize('UA-165948384-1'); // Google Analytics
   return (
     // <SignIn />
     <BrowserRouter basename={process.env.PUBLIC_URL}>
